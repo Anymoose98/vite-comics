e49ef0d074ade9595dc7c53960f308d51ecc6b31
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-01-08 19:30:19 +0100

    header fatto

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -1,23 +1,92 @@
 <script>
 export default {
-
+    name: 'appHeader',
+    data() {
+        return {
+            menu: [
+                {
+                    label: 'CHARACTERS'
+                },
+                {
+                    label: 'COMICS'
+                },
+                {
+                    label: 'MOVIES'
+                },
+                {
+                    label: 'TV'
+                },
+                {
+                    label: 'GAMES'
+                },
+                {
+                    label: 'COLLECTIBLES'
+                },
+                {
+                    label: 'VIDEOS'
+                },
+                {
+                    label: 'FANS'
+                },
+                {
+                    label: 'NEWS'
+                },
+                {
+                    label: 'SHOP'
+                },
+            ]
+        }
+    }
 }
 </script>
 
 <template lang="">
     <header>
-        <div>
+        <div class="col">
             <img src="../assets/img/dc-logo.png" alt="">
         </div>
         <div>
-            menu
+            <nav>
+                <ul>
+                    <li v-for="(label, index) in menu" :key="index" >
+                        <a href="">{{menu[index].label}}</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </header>
 </template>
+
+
 <style lang="scss" scoped>
 header {
-    max-width: 1000px;
+    max-width: 1200px;
     margin: 0 auto;
     display: flex;
+    margin-top: 20px;
+}
+
+ul {
+    list-style-type: none;
+}
+
+li {
+    display: inline-block;
+    margin: 10px;
+    font-weight: bold;
+    font-size: small;
+}
+
+a {
+    text-decoration: none;
+    color: black;
+}
+
+a:hover {
+    color: rgb(35, 146, 250)
+}
+
+.col {
+    width: 30%;
 }
 </style>
