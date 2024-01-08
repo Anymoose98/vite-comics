757a9820a5034aabd74cb32e9b618d4118d737e7
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-01-08 18:53:05 +0100

    spostato img

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script>
+import appHeader from './components/appHeader.vue';
 export default {
-
+  components: {
+    appHeader
+  }
 }
 </script>
 
 <template lang="">
   <div>
-    
+    <appHeader/>
   </div>
 </template>
 <style lang="scss">
---
+++ src/components/appHeader.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+
+}
+</script>
+
+<template lang="">
+    <header>
+        <div>
+            <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+        <div>
+            menu
+        </div>
+    </header>
+</template>
+<style lang="scss" scoped>
+header {
+    max-width: 1000px;
+    margin: 0 auto;
+    display: flex;
+}
+</style>
